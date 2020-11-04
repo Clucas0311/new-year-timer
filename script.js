8e4315aeab4f7a8783bcb1484075f42486b5f040
Clucas0311 <clucas0311@gmail.com>
2020-11-04 17:58:54 -0500

    Grabbing elements for DOM

---
+++ script.js
@@ -0,0 +1,4 @@
+const days = document.querySelector('#days');
+const days = document.querySelector('#hours');
+const days = document.querySelector('#minutes');
+const days = document.querySelector('#seconds');
