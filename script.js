7781cc3b9aff2f9c4e56dd5547abce9c768a0111
Clucas0311 <clucas0311@gmail.com>
2020-11-04 19:02:53 -0500

    corrected the script tag

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
+const countdown = document.querySelector('#countdown');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+	const currentTime = new Date();
+	const diff = newYearTime - currentTime;
+
+	const day = Math.floor(diff / 1000 / 60 / 60 / 24);
+	const hour = Math.floor(diff / 1000 / 60 / 60) % 24;
+	const min = Math.floor(diff / 1000 / 60) % 60;
+	const sec = Math.floor(diff / 1000 / 60) % 60;
+
+	days.innerHTML = day;
+	hours.innerHTML = hour < 10 ? '0' + hour : hour;
+	minutes.innerHTML = hour < 10 ? '0' + min : min;
+	hours.innerHTML = hour < 10 ? '0' + sec : sec;
+}
+
+setInterval(updateCountdown, 1000);
